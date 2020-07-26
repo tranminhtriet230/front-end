0a6b11adccb59fbd4c1180c034e1b672b07c9661
tranminhtriet230 <68809460+tranminhtriet230@users.noreply.github.com>
2020-07-26 18:01:25 +0700

    Add files via upload

---
+++ public/js/sitescript.js
@@ -0,0 +1,12 @@
+// jQuery(document).ready(function($){
+//     pos= $(".menu-top").position();
+
+//     $(window).scroll(function(){
+//         var posScroll=$(document).scrollTop();
+//         if(parseInt(posScroll)>parseInt(pos.top)){
+//             $(".menu-top").addClass('fixed');
+//         }else{
+//             $(".menu-top").removeClass('fixed');
+//         }
+//     })
+// })
